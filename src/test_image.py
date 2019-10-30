b7b032d8c8ff4c03e8bada9605b9cc1ca843a4a5
salo1893 <salo.mueller@stud.unibas.ch>
2019-10-30 18:11:40 +0100

    made tests nicer (assertEqual instead of assertTrue)

--- src/test_image.py
+++ src/test_image.py
@@ -6,7 +6,7 @@ class TestImage(TestCase):
     def test_levelUp0(self):
         image = Image(0)
         image.levelUp()
-        self.assertTrue(image.level == 0)
+        self.assertEqual(image.level, 0)
 
     def test_levelUp1(self):
         image = Image(0)
@@ -14,7 +14,7 @@ class TestImage(TestCase):
         image.addTag('b')
         image.addTag('c')
         image.printTags()
-        self.assertTrue(image.level == 1)
+        self.assertEqual(image.level, 1)
 
     def test_levelUp2(self):
         image = Image(0)
@@ -23,32 +23,32 @@ class TestImage(TestCase):
         image.addTag('c')
         image.addTag('d')
         image.addTag('e')
-        self.assertTrue(image.level == 2)
+        self.assertEqual(image.level, 2)
 
     def test_addTag_first(self):
         image = Image(0)
-        self.assertTrue(image.addTag('a') == 1)
+        self.assertEqual(image.addTag('a'), 1)
 
     def test_addTag_second(self):
         image = Image(0)
-        self.assertTrue(image.addTag('a') == 1)
-        self.assertTrue(image.addTag('b') == 1)
+        self.assertEqual(image.addTag('a'), 1)
+        self.assertEqual(image.addTag('b'), 1)
 
     def test_addTag_sameTag(self):
         image = Image(0)
-        self.assertTrue(image.addTag('a') == 1)
-        self.assertTrue(image.addTag('b') == 1)
-        self.assertTrue(image.addTag('b') == 1)
-        self.assertTrue(image.getTag('b').getFrequency() == 2)
+        self.assertEqual(image.addTag('a'), 1)
+        self.assertEqual(image.addTag('b'), 1)
+        self.assertEqual(image.addTag('b'), 1)
+        self.assertEqual(image.getTag('b').getFrequency(), 2)
 
     def test_addTag_level1(self):
         image = Image(0)
         image.addTag('a')
         image.addTag('b')
-        self.assertTrue(image.addTag('c') == 1)
-        self.assertTrue(image.level == 1)
-        self.assertTrue(image.addTag('c') == 2)
-        self.assertTrue(image.addTag('d') == 1)
+        self.assertEqual(image.addTag('c'), 1)
+        self.assertEqual(image.level, 1)
+        self.assertEqual(image.addTag('c'), 2)
+        self.assertEqual(image.addTag('d'), 1)
 
     def test_addTag_level2(self):
         image = Image(0)
@@ -56,29 +56,29 @@ class TestImage(TestCase):
         image.addTag('b')
         image.addTag('c')
         image.addTag('d')
-        self.assertTrue(image.addTag('e') == 2)
-        self.assertTrue(image.level == 2)
-        self.assertTrue(image.addTag('a') == 0)
-        self.assertTrue(image.addTag('c') == 2)
+        self.assertEqual(image.addTag('e'), 2)
+        self.assertEqual(image.level, 2)
+        self.assertEqual(image.addTag('a'), 0)
+        self.assertEqual(image.addTag('c'), 2)
 
     def test_validate_wrong(self):
         image = Image(0)
-        self.assertTrue(image.validate('blublio') == -1)
+        self.assertEqual(image.validate('blublio'), -1)
 
     def test_validate_misspelled(self):
         image = Image(0)
-        self.assertTrue(image.validate('ertz') != -1)
+        self.assertNotEqual(image.validate('ertz'), -1)
 
     def test_validate_uppercase(self):
         image = Image(0)
         image.addTag('SHERLOCK')
-        self.assertTrue(image.getTag('sherlock') != None)
+        self.assertNotEqual(image.getTag('sherlock'), None)
 
     def test_validate_twoWords(self):
         image = Image(0)
-        self.assertTrue(image.addTag('Sherlock Holmes') == 1)
-        self.assertTrue(image.getTag('Sherlock Holmes') != None)
-        self.assertTrue(image.addTag('Sherlock Holmes and Watson') == 0)
+        self.assertEqual(image.addTag('Sherlock Holmes'), 1)
+        self.assertNotEqual(image.getTag('Sherlock Holmes'), None)
+        self.assertEqual(image.addTag('Sherlock Holmes and Watson'), 0)
 
 
     def test_getTag(self):
@@ -89,12 +89,12 @@ class TestImage(TestCase):
         image.addTag('c')
         image.addTag('d')
         tag = image.getTag('b')
-        self.assertTrue(tag.getFrequency() == 2)
-        self.assertTrue(tag.getWord() == 'b')
+        self.assertEqual(tag.getFrequency(), 2)
+        self.assertEqual(tag.getWord(), 'b')
         tag = image.getTag('c')
-        self.assertTrue(tag.getFrequency() == 1)
-        self.assertTrue(tag.getWord() == 'c')
+        self.assertEqual(tag.getFrequency(), 1)
+        self.assertEqual(tag.getWord(), 'c')
 
     def test_getTag_invalid(self):
         image = Image(0)
-        self.assertTrue(image.getTag('a') == None)
+        self.assertEqual(image.getTag('a'), None)
