ee193abf8f81ee4b5f5b5fd64a5a557938ddc50b
witjon <witjon@users.noreply.gitlab.com>
2019-11-04 15:13:14 +0100

    Add 4xx error page

--- src/acomp/routes.py
+++ src/acomp/routes.py
@@ -1,8 +1,24 @@
-from flask import send_from_directory, render_template
+from flask import render_template
 from acomp import app
-from acomp.models import Image, Tag, User
 
 
 @app.route('/')
-def index():
-    return render_template('index.html', source = '/static/img/test.png', width = 800, height = 600)
+@app.route('/home')
+@app.route('/classic')
+def classic():
+    return render_template('index.html', source='/static/img/test.png', width=800, height=600)
+
+
+@app.errorhandler(400)
+def bad_request(e):
+    return render_template('4xx.html', error_msg=e), 400
+
+
+@app.errorhandler(401)
+def forbidden(e):
+    return render_template('4xx.html', error_msg=e), 401
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('4xx.html', error_msg=e), 404
