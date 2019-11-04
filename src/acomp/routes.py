94f8059a5639dd4f9ebe49def714bfaface3f3cc
witjon <witjon@users.noreply.gitlab.com>
2019-11-04 17:28:52 +0100

    Add static dummy API endpoint

--- src/acomp/routes.py
+++ src/acomp/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, url_for
 from acomp import app
 
 
@@ -6,7 +6,29 @@ from acomp import app
 @app.route('/home')
 @app.route('/classic')
 def classic():
-    return render_template('index.html', source='/static/img/test.png', width=800, height=600)
+    return render_template('index.html', source=url_for('static', filename='img/test.png'), width=800, height=600)
+
+
+@app.route('/classic/data', methods=['GET'])
+def classic_data_get():
+    data: dict = {}
+    data['images'] = url_for('static', filename='img/test.png')
+    data['timelimit'] = app.config['ACOMP_CLASSIC_TIMELIMIT']
+    data['accepted'] = ''
+    data['score'] = '0'
+    data['user'] = '0'
+
+    res = app.make_response(data)
+    res.headers.set('Content-Type', 'application/json')
+    # return Response(data, mimetype='application/json')
+    return res
+
+
+@app.route('/classic/data', methods=['POST'])
+def classic_data_post(request):
+    # TODO: check for user/token
+    data = request.get_json()
+    return '{"OK":"200"}'
 
 
 @app.errorhandler(400)
