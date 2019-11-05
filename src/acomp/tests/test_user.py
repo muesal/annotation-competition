c1308762ede0c81d9ca09c8d4fed9816a5b9ed11
salo1893 <salo.mueller@stud.unibas.ch>
2019-11-05 10:43:27 +0100

    tests for users running

--- src/acomp/tests/test_user.py
+++ src/acomp/tests/test_user.py
@@ -1,18 +1,18 @@
 from unittest import TestCase
-from acomp.Game_logic import Image, User, Tag
+from acomp.gamelogic import GLImage, GLUser, GLTag
 
 
 class TestUser(TestCase):
     def test_tagImage(self):
-        user = User(0)
-        image = Image(0)
+        user = GLUser('franz')
+        image = GLImage(1)
         user.tagImage(image, 'first')
         self.assertEqual(user.getScore(), 1)
-        user.tagImage(image, 'first')
+        user.tagImage(image, 'second')
         self.assertEqual(user.getScore(), 2)
         self.assertEqual(image.tags[0].getWord(), 'first')
-        self.assertEqual(len(image.tags), 1)
+        self.assertEqual(len(image.tags), 2)
 
     def test_getScore(self):
-        user = User(0)
+        user = GLUser('franz')
         self.assertEqual(user.getScore(), 0)
