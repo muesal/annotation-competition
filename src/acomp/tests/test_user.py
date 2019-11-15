66bbb55a6650408b67dc93a6b5d3745f93c9f311
salo1893 <salo.mueller@stud.unibas.ch>
2019-11-15 15:31:30 +0100

    test startClassic of GLUser

--- src/acomp/tests/test_user.py
+++ src/acomp/tests/test_user.py
@@ -10,6 +10,11 @@ class TestUser(TestCase):
         self.image_id = 3
         self.image = GLImage(self.image_id)
 
+    def test_startClassic(self):
+        image_id = self.user.startClassic()
+        self.assertIsNotNone(image_id)
+        GLImage(image_id)
+
     def test_tagImage(self):
         self.user.tagImage('first', self.image)
         self.assertEqual(self.user.getScore(), 1)
@@ -17,3 +22,6 @@ class TestUser(TestCase):
         self.assertEqual(self.user.getScore(), 2)
         self.assertEqual(self.image.tags[0].getWord(), 'first')
         self.assertEqual(len(self.image.tags), 2)
+
+    def test_end(self):
+        self.assertIsNotNone(self.user.end())
