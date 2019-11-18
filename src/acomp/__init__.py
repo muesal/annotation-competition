283f99228648d6692f2ecfd010585414ccae1718
witjon <witjon@users.noreply.gitlab.com>
2019-11-18 01:55:10 +0100

    Add flask session

--- src/acomp/__init__.py
+++ src/acomp/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
 
 app = Flask(__name__, template_folder='templates')
 app.config.from_object('config')
@@ -8,6 +9,7 @@ app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+sessions = Session(app)
 
 from acomp import routes
 from acomp import prefill
--- src/acomp/routes.py
+++ src/acomp/routes.py
@@ -1,37 +1,45 @@
-from flask import render_template, request, url_for
-from acomp import app
+from flask import render_template, request, session, url_for
+from acomp import app, glUser, sessions
 
 
 @app.route('/')
 @app.route('/home')
 @app.route('/classic')
 def classic():
+    if 'user' in session:
+        print(session['user'])
+    else:
+        session['user'] = 'guest'
     return render_template('index.html', source=url_for('static', filename='img/test.png'), width=800, height=600)
 
 
 @app.route('/classic/data', methods=['GET'])
 def classic_data_get():
-    data: dict = {}
-    data['images'] = url_for('static', filename='img/test.png')
-    data['timelimit'] = app.config['ACOMP_CLASSIC_TIMELIMIT']
-    data['accepted'] = ''
-    data['score'] = '0'
-    data['user'] = '0'
+    if 'user' in session:
+        data: dict = {}
+        data['images'] = url_for('static', filename='img/test.png')
+        data['timelimit'] = app.config['ACOMP_CLASSIC_TIMELIMIT']
+        data['accepted'] = ''
+        data['score'] = '0'
+        data['user'] = '0'
 
-    res = app.make_response(data)
-    res.headers.set('Content-Type', 'application/json')
-    # return Response(data, mimetype='application/json')
-    return res
+        res = app.make_response(data)
+        res.headers.set('Content-Type', 'application/json')
+        return res
+    else:
+        return forbidden('Not authorized.')
 
 
 @app.route('/classic/data', methods=['POST'])
 def classic_data_post():
-    # TODO: check for user/token
-    data = request.get_json()
-    if data is None:
-        return bad_request('Invalid JSON.')
+    if 'user' in session:
+        data = request.get_json()
+        if data is None:
+            return bad_request('Invalid JSON.')
+        else:
+            return '{"OK":"200"}'
     else:
-        return '{"OK":"200"}'
+        return forbidden('Not authorized.')
 
 
 @app.errorhandler(400)
