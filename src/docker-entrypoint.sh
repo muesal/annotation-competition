740a22e10cb50de32a2ef959c14b495ece192f7b
witjon <witjon@users.noreply.gitlab.com>
2020-01-24 20:24:27 +0100

    Add initialisation

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 PREFILL_DIR="prefill"
 
 if [ "$1" = 'flask' ] || [ "$1" = "uwsgi" ]; then
-	flask db upgrade
+	flask db init || flask db migrate || echo "skipping..."
 	flask nltk-data
 	if [ -d "$PREFILL_DIR" ]; then
 		flask prefill "$PREFILL_DIR"
