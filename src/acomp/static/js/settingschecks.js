3f6ce1b2b2555bf4ba88db4c5837ffd1e1360472
Andreas Meier <andreas.meier@unibas.ch>
2020-01-25 07:31:55 +0100

    Make checks for signup and settings DRYer

---
+++ src/acomp/static/js/settingschecks.js
@@ -0,0 +1,86 @@
+'use strict';
+
+function writeToJson(username) {
+    const obj = {
+        name: username,
+    };
+    return JSON.stringify(obj);
+}
+
+async function checkLoginname(e) {
+    const payload = writeToJson(loginname.value);
+
+    try {
+        const response = await fetch(requestUrl, {
+            method: 'POST',
+            body: payload,
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrf_token.value,
+            }
+        });
+        console.log('Sent:', payload);
+        if (response.ok) {
+            const json = await response.json();
+            console.log('Success:', JSON.stringify(json));
+            if (json.available != '1') {
+                loginname.setCustomValidity('This username is already taken, please choose another name.');
+                return;
+            } else {
+                loginname.setCustomValidity("");
+            }
+        } else {
+            console.error('Error:', response.statusText); // TODO: notify user
+        }
+    } catch (err) {
+        console.error('Error:', err);
+    }
+}
+
+async function checkPasswords(e) {
+    passwordConfirm.setCustomValidity('');
+    if (password.value !== passwordConfirm.value) {
+        passwordConfirm.setCustomValidity('Please make sure to confirm your password');
+        return;
+    } else {
+        passwordConfirm.setCustomValidity('');
+    }
+
+    // https://haveibeenpwned.com/API/v3#SearchingPwnedPasswordsByRange
+    const apiurl = 'https://api.pwnedpasswords.com/range/';
+    const hash = await digest(password.value);
+    const fetchurl = apiurl + hash.substring(0, 5);
+    var regex = new RegExp('^' + hash.slice(5) + ':(\\d+)', 'im');
+    console.log('Search for:', regex.source);
+
+    try {
+        const response = await fetch(fetchurl);
+        console.log('Sent:', fetchurl);
+        if (response.ok) {
+            const data = await response.text();
+            console.log('Success:', data);
+            if (regex.test(data)) {
+                paragraph.textContent += "Your password might be insecure";
+                return;
+            } else {
+                console.log('Not yet been pwned.');
+                paragraph.textContent = "";
+            }
+        } else {
+            console.error('Error:', response.statusText); // TODO: notify user
+        }
+    } catch (err) {
+        console.error('Error:', err);
+    }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#Converting_a_digest_to_a_hex_string
+async function digest(str) {
+    const strUint8 = new TextEncoder().encode(str);
+    const hashBuffer = await crypto.subtle.digest('SHA-1', strUint8);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    console.log('SHA-1:', hashHex);
+    return hashHex;
+}
+
