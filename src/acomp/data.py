5417530d39ffe8a785d503eb8a967b18be6affec
witjon <witjon@users.noreply.gitlab.com>
2020-01-27 09:11:49 +0100

    Set default value for NLTK_DATA

--- src/acomp/data.py
+++ src/acomp/data.py
@@ -5,4 +5,5 @@ from acomp import app
 
 @app.cli.command("nltk-data")
 def data():
-    nltk.download('popular', download_dir=os.environ['NLTK_DATA'])
+
+    nltk.download('popular', download_dir=os.getenv('NLTK_DATA', app.config['NLTK_DATA']))
