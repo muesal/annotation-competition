ff67140794c512cd981e995ef32b425d06b59e8b
salo1893 <salo.mueller@stud.unibas.ch>
2020-01-27 11:22:26 +0100

    removed unneccessary nltk download

--- src/acomp/data.py
+++ src/acomp/data.py
@@ -6,4 +6,3 @@ from acomp import app
 @app.cli.command("nltk-data")
 def data():
     nltk.download('popular', download_dir=os.getenv('NLTK_DATA', app.config['NLTK_DATA']))
-    nltk.download('wordnet', download_dir=os.getenv('NLTK_DATA', app.config['NLTK_DATA']))
