6ad2a2c0320c1050504b6f194322005a8c47cc7c
witjon <witjon@users.noreply.gitlab.com>
2020-01-30 17:01:54 +0100

    Fix order of execution

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 PREFILL_DIR="prefill"
 
 if [ "$1" = 'flask' ] || [ "$1" = "uwsgi" ]; then
-	flask db upgrade
 	# The flask initialisation is currently broken without any data
 	flask nltk-data || python -m nltk.downloader -d ${NLTK_DATA:=/usr/share/nltk_data} all
+	flask db upgrade
 	if [ -d "$PREFILL_DIR" ]; then
 		flask prefill "$PREFILL_DIR"
 	fi
